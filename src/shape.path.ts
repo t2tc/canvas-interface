0b742b452ffe9272879b536cb9559ade715cdc33
t2tc <t2tc@protonmail.com>
2025-09-15 02:07:00 +0800

    there will be no more bounding box calc for path now.

--- src/shape.path.ts
+++ src/shape.path.ts
@@ -3,7 +3,7 @@ import { Shape } from './shape.base';
 class Path extends Shape {
     private path2D: Path2D;
 
-    constructor(svgPath: string) {
+    constructor(svgPath: string, svgViewBox: [number, number, number, number], size: [number, number]) {
         super();
         this.path2D = new Path2D(svgPath);
     }
